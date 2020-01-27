d64903fcf073756488003e686c0b07624fb9db5f
apenahli <apenahli001@gmail.com>
2020-01-27 11:06:18 +0400

    add new project

---
+++ spring-boot-swagger-example-master/src/main/java/com/techprimers/springboot/swaggerexample/SwaggerExampleApplication.java
@@ -0,0 +1,15 @@
+package com.techprimers.springboot.swaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerExampleApplication.class, args);
+	}
+}
+
+
+//https://github.com/hendisantika/spring-boot-rest-swagger-example
